322c5927e78ab00a24f2066d14418260c71b2849
Wandia E Mugo <wwandia173@gmail.com>
2022-06-07 21:04:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet<= 400 ) {
+    return ("This one is on me!")
+  } else if (feet >2000 && feet<2500){
+     return ("I will gladly take your thirty bucks.")
+  } else if (feet >2500){
+    return ("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
+scuberGreetingForFeet(199)
+scuberGreetingForFeet(2001)
+scuberGreetingForFeet(2501)
+
+function ternaryCheckCity(city){
   // Write your code here!
+   const result = (city === "NYC") ?  "Ok, sounds good." : "No go.";
+   return result;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+       break;
+  }
 }
